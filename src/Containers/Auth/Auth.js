b9963613b279c7be23b9651bd5eb0c35f2153433
AndreiKlimovich <andrei.klimovich84@gmail.com>
2020-11-19 18:57:02 +0300

    Auth validation finished

--- src/Containers/Auth/Auth.js
+++ src/Containers/Auth/Auth.js
@@ -1,7 +1,129 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classes from './Auth.module.scss';
+import Button from '../../Components/UI/Button/Button';
+import Input from '../../Components/UI/Input/Input';
 
-const Auth = () => {
-  return <div>Auth</div>;
-};
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+class Auth extends Component {
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
+
+  loginHandler = () => {};
+  registerHandler = () => {};
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+  validateControl = (value, validation) => {
+    if (!validation) {
+      return true;
+    }
+    let isValid = true;
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (validation.email) {
+      isValid = validateEmail(value) && isValid;
+    }
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  };
+  onChangeHandler = (event, controlName) => {
+    const copyFormControls = { ...this.state.formControls };
+    const control = { ...copyFormControls[controlName] };
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+
+    copyFormControls[controlName] = control;
+
+    let isFormValid = true;
+    Object.keys(copyFormControls).forEach((name) => {
+      isFormValid = copyFormControls[name].valid && isFormValid;
+    });
+
+    this.setState({
+      formControls: copyFormControls,
+      isFormValid,
+    });
+  };
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={(event) => this.onChangeHandler(event, controlName)}
+        />
+      );
+    });
+  }
+  render() {
+    return (
+      <div className={classes.Auth}>
+        <div className={classes.wrapper}>
+          <h1>Авторизация</h1>
+          <form onSubmit={this.submitHandler}>
+            {this.renderInputs()}
+            <Button
+              type='success'
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Войти
+            </Button>
+            <Button
+              type='primary'
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Auth;
--- src/Containers/QuizCreator/QuizCreator.js
+++ src/Containers/QuizCreator/QuizCreator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const QuizCreator = () => {
+const QuizCreator = (props) => {
   return <div>QuizCreator</div>;
 };
 
